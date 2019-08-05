48de87b3e1d30fd1383dd0a410e119d63139f95b
dansigit <e.dansi.111@gmail.com>
2019-08-05 12:28:17 +0430

    newcommit

--- app/src/main/java/com/example/mypc/newproject/MainActivity.java
+++ app/src/main/java/com/example/mypc/newproject/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(getApplicationContext(),"message",Toast.LENGTH_LONG).show();
+        Toast.makeText(getApplicationContext(),"my",Toast.LENGTH_LONG).show();
 
     }
 }
