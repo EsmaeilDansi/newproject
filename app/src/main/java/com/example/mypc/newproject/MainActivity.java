4f36537bc7e7098a0ee67e3a71861b26d69198dc
dansigit <e.dansi.111@gmail.com>
2019-08-05 12:25:04 +0430

    newcommit

--- app/src/main/java/com/example/mypc/newproject/MainActivity.java
+++ app/src/main/java/com/example/mypc/newproject/MainActivity.java
@@ -2,12 +2,17 @@ package com.example.mypc.newproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(getApplicationContext(),"message",Toast.LENGTH_LONG).show();
+
     }
 }
